bf2b1358559db0c4eb2e5afb6c53fab09181ef3a
Nethra1619 <nnethra934@gmail.com>
2024-09-14 10:57:39 +0530

    Create example.py

---
+++ example.py
@@ -0,0 +1,19 @@
+# List example
+my_list = [1, 2, 3, 4]
+my_list.append(5)
+print("Appended 5 to list:", my_list)
+
+# Tuple example
+my_tuple = (1, 2, 3)
+new_tuple = my_tuple + (4,)
+print("Concatenated tuple:", new_tuple)
+
+# Dictionary example
+my_dict = {'name': 'John', 'age': 30}
+my_dict['job'] = 'Developer'
+print("Added job to dictionary:", my_dict)
+
+# Set example
+my_set = {1, 2, 3}
+my_set.add(4)
+print("Added 4 to set:", my_set)
